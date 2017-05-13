2c84056f0d9ceabc9835c2d131450708a7e35b3a
Kiti <zesani@Kitis-MacBook-Pro.local>
2017-05-13 10:18:35 +0700

    add get location

--- src/pages/Report.vue
+++ src/pages/Report.vue
@@ -2,11 +2,13 @@
   <div class="">
     <h1>Report</h1>
     เรื่อง<input type="text" v-model="topic"><br>
+    <button type="button" @click="getLocation">ใช้ข้อมูลตำแหน่ง</button>
+    {{ locationGps }}<br>
     สถานที่เกิดเหตุ<input type="text" v-model="location"><br>
     รายละเอียด<input type="text" v-model="description"><br>
     ชื่อ-สกุล<input type="text" v-model="fullName"><br>
     เบอร์โทร<input type="text" v-model="phone"><br>
-    <button type="button" @click="add (topic, location, description, fullName, phone)">เพิ่มปัญหา</button>
+    <button type="button" @click="add (topic, locationGps, location, description, fullName, phone)">เพิ่มปัญหา</button>
   </div>
 </template>
 <script>
@@ -16,6 +18,7 @@ export default {
     return {
       topic: '',
       location: '',
+      locationGps: '',
       fullName: '',
       phone: '',
       description: ''
@@ -23,19 +26,32 @@ export default {
   },
   methods: {
     ...mapActions(['addIssue']),
-    add (topic, location, description, fullName, phone) {
+    add (topic, locationGps, location, description, fullName, phone) {
       this.addIssue({
         topic,
+        locationGps,
         location,
         description,
         fullName,
         phone
       })
       this.topic = ''
+      this.locationGps = ''
       this.location = ''
       this.description = ''
       this.fullName = ''
       this.phone = ''
+    },
+    getLocation () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.addLocation)
+      } else {
+        console.log('Geolocation is not supported by this browser.')
+      }
+    },
+    addLocation (position) {
+      var location = [position.coords.latitude, position.coords.longitude]
+      this.locationGps = location
     }
   }
 }
