f7931ebc80eca5ae10957984ed773ec689febfcf
Kiti <zesani@Kitis-MacBook-Pro.local>
2017-05-13 14:36:56 +0700

    edit home show issue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,14 @@
 <template lang="html">
   <div class="">
     <h1>Home</h1>
-      <div v-for="issue in issues">{{ issue }}
+      <div v-for="issue in issues">
+        <ul>
+          <li>topic: {{issue.topic}}</li>
+          <li>GPS: {{issue.locationGps}}</li>
+          <li>สถานที่​: {{issue.location}}</li>
+          <li>ชื่อ: {{issue.fullName}}</li>
+          <li>โทรซ: {{issue.phone}}</li>
+        </ul>
         <img v-for="photo in issue.photos" :src="photo.img" alt="">
       </div>
 
